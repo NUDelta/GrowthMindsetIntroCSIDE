2fb47176f3bb5a1629d3fab505d5553112aa6405
Jamie Gorson <jamie@gorsonfamily.com>
2017-09-27 15:50:57 -0500

    implemented lowe level heuristic detection and 2 metrics
    paste metric and error click method

--- main.js
+++ main.js
@@ -1,65 +1,142 @@
-
-// $(document).ready(function () {
-//     console.log("hi");
-//     Selection.on("changeSelection", function(){
-//         console.log('select');
-//     });
-// });
-
-console.log(CodeMirror);
-// var editor = document.createElement('div')
-// editor.className = 'editor'
-
-//     var button = document.createElement('div')
-//     button.className = 'editor-button'
-//     button.innerHTML = 'Run'
-// editor.appendChild(button)
-
-// var editorArea = document.createElement('textarea')
-// editorArea.className = 'editor-area'
-// editorArea.id = "hi"
-// editor.appendChild(editorArea)
-
-var myCodeMirror = CodeMirror(document.body, {
-  value: "function myScript(){return 100;}\n",
+var mousedown = false;
+var lastCompilation;
+var lineError = '';
+var myCodeMirror;
+var charCount;
+$(document).ready(function(e) {
+  //set up screen
+  myCodeMirror = CodeMirror.fromTextArea(document.getElementById('code'), {
   mode:  "python",
-  theme: 'monokai'
+  theme: 'monokai',
+  });
+
+  var charCount = myCodeMirror.getValue().length;
+  //Run Button listener
+  $('#runButton').onclick = function(e) {
+   runit(myCodeMirror);
+  }
+  // CodeMirror Listeners
+  myCodeMirror.on('change',function(cMirror, change){
+    editorChange(change);
+  });
+  myCodeMirror.on('cursorActivity',function(cMirror){
+    cursorChange(cMirror);
+  });
+  myCodeMirror.on("mousedown", function () {
+    mousedown = true;
+  });
 });
 
-var button = document.createElement('div')
-button.className = 'editor-button'
-button.innerHTML = 'Run'
-document.body.appendChild(button)
 
-button.onclick = function(e) {
-	console.log(myCodeMirror.getValue());
-	// $.ajax({
- //   url: "/path/to/your/script",
- //   success: function(response) {
-     // here you do whatever you want with the response variable
-   // }
-// });
-    // var eid = challenge.name;
-    // resetMessages(this.parentElement);
-    // try {
-    //     var fn = execute(signature.innerHTML, cmEditor.getValue());
-    //     challenge.implementation = fn;
-    //     var results = challenge.runTests();;
-    //     displayResults(results, this.parentElement);
-    // } catch (e) {
-    //     displayError(e.message, this.parentElement);
-    // }
+/**
+ * Function outf:()
+ * input: output text from python run
+ * output: none
+ * Displays output text in html output div as a new paragraph
+*/
+function outf(text) { 
+  if (!text.trim()){}
+  else{
+    var mypre = document.getElementById("output"); 
+    mypre.innerHTML = mypre.innerHTML + "\n"+text ; 
+  }
+} 
+/**
+ * Function builtinRead:()
+ * input: x
+ * output: none
+ * reader for running python code
+*/
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+            throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
 }
-// var cmEditor = CodeMirror.fromTextArea(editorArea, {
-//     mode: 'python',
-//     lineWrapping: true,
-//     lineNumbers: true,
-//     theme: 'monokai',
-//     smartIndent: false,
-//     electricChars: false
-// });
 
-// cmEditor.refresh();
+/**
+ * function runit()
+ * inputs/outputs: codeMirror object
+ * runs the python code that is contained in the CodeMirror and displays the errors or output
+*/
+var lastSetTimeout;
+function runit(myCodeMirror) {
+   var d = new Date(); 
+   d.getTime();
+   //how to get time between compilations
+   //console.log(d- lastCompilation);
+   lastCompilation = d;
+   var prog = myCodeMirror.getValue(); 
+   var mypre = document.getElementById("output"); 
 
-// document.body.appendChild( editor );
-console.log(document);
+   Sk.pre = "output";
+   // this should realy be after promise is fullfilled, but i'm not sure how to do that when i want it to go after all the outputs
+   mypre.innerHTML = mypre.innerHTML +  "\n>"; 
+
+   Sk.configure({output:outf, read:builtinRead}); 
+   var myPromise = Sk.misceval.asyncToPromise(function() {
+       return Sk.importMainWithBody("<stdin>", false, prog, true);
+   });
+   myPromise.then(function(mod) {
+       console.log('success');
+       $('#output').scrollTop($('#output')[0].scrollHeight);
+   },
+   function(err) {
+       outf(err.toString());
+       console.log(err);
+       clearTimeout(lastSetTimeout);
+       lineError = getErrLineNum(err);
+       lastSetTimeout = setTimeout(function(){ 
+        lineError = '';
+        console.log('reset LineNum') 
+    }, 30000)
+       $('#output').scrollTop($('#output')[0].scrollHeight);
+   });
+     
+} 
+
+function editorChange(changeObj) {
+  if (changeObj.from.line == changeObj.to.line && changeObj.from.ch == changeObj.to.ch){
+    console.log('same');
+  }
+  newCharCount = myCodeMirror.getValue().length;
+  if (newCharCount-charCount>2){
+    console.log('METRIC: paste')
+  }
+  charCount = newCharCount;
+  console.log(lineError);
+  if (lineError){
+    if (Math.abs(changeObj.to.line - lineError) <3){
+      lineError=''
+      console.log("METRIC edit Line num")
+    }
+  }
+}
+
+function cursorChange(cMirror) {
+   sel = myCodeMirror.getSelection(); //literally a string of what is selected
+    if (sel){
+      //console.log('selection');
+    }
+    else {
+      //console.log('nosel');
+      //console.log(myCodeMirror.getCursor());
+      if (mousedown == true) {
+        //user clicked on the 
+        //console.log("click");
+      }
+    }
+    mousedown = false;
+}
+
+document.onkeypress = function (event) {
+  // console.log(event);
+}
+
+/*  getErrLineNum
+    inputs: msg - string of error message
+    output: number of line error occured on
+*/
+function getErrLineNum(err) {
+  var lineNum = err.traceback[0].lineno;
+  return lineNum
+}
