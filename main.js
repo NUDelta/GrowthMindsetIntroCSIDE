1bcf170ec85377ecaec56501d3f5523b11b8a943
Jamie Gorson <jamie@gorsonfamily.com>
2017-10-02 13:22:30 -0500

    added check for print

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var lineError = '';
 var myCodeMirror;
 var charCount;
 var lastKeyPressed;
+var lastCompileSuccessful;
 
 
 $(document).ready(function(e) {
@@ -34,6 +35,43 @@ $('#runButton').on('click', function(e) {
   });
 });
 
+
+/**
+ * Function checkForPrint
+ * input: prog - text executed in program
+ * output: none
+ * Checks to see if uses new print statement for debugging
+**/
+  preEstablishedPrints = ["print midpoint(1,3,4,1)", "print takeOutNeg([2,-1,3,-5,0,1])", "print takeOutPos([2,-1,3,-5,0,1])"]
+  var lastPrints = [];
+  function checkForPrint(prog) {
+    stillPrints = true;
+    lastPrint = 0;
+    var thisPrints = [];
+
+    while(stillPrints){
+      // console.log('lastPrint'+lastPrint)
+      i = prog.indexOf("print", lastPrint)
+      lastPrint = i+1
+      if (i== -1){
+        stillPrints = false;
+      }
+      else {
+        printState = prog.substr(i).split("\n", 1)[0]
+        if (($.inArray(printState, preEstablishedPrints))<0){
+          if(($.inArray(printState, lastPrints))<0){
+            if (lastCompileSuccessful == false) {
+              console.log("metric NEWPRINT")
+            }
+          }
+          thisPrints.push(printState)
+        }
+      }
+    }
+    $.each(thisPrints, function(i, el){
+      lastPrints.push(el);
+    });
+  }
 
 /**
  * Function outf:()
@@ -68,25 +106,27 @@ function builtinRead(x) {
 var lastSetTimeout;
 function runit(myCodeMirror) {
   //get time between compilations
-   var d = new Date(); 
-   d.getTime();
-   var compileDelta = d - lastCompilation;
-   lastCompilation = d;
+    var d = new Date(); 
+    d.getTime();
+    var compileDelta = d - lastCompilation;
+    lastCompilation = d;
 
-   //run python code using skuplt
-   var prog = myCodeMirror.getValue(); 
-   var mypre = document.getElementById("output"); 
-   Sk.pre = "output";
-   Sk.configure({output:outf, read:builtinRead}); 
-   var myPromise = Sk.misceval.asyncToPromise(function() {
+    //run python code using skuplt
+    var prog = myCodeMirror.getValue(); 
+    var mypre = document.getElementById("output"); 
+    Sk.pre = "output";
+    Sk.configure({output:outf, read:builtinRead}); 
+    var myPromise = Sk.misceval.asyncToPromise(function() {
        return Sk.importMainWithBody("<stdin>", false, prog, true);
-   });
-   myPromise.then(function(mod) {
+    });
+    checkForPrint(prog);
+    myPromise.then(function(mod) {
        console.log('success');
        mypre.innerHTML = mypre.innerHTML +  "\n>"; 
        $('#output').scrollTop($('#output')[0].scrollHeight);
-   },
-   function(err) {
+       lastCompileSuccessful = true;
+    },
+    function(err) {
        outf(err.toString());
        console.log(err);
        clearTimeout(lastSetTimeout);
@@ -96,8 +136,8 @@ function runit(myCodeMirror) {
     }, 30000)
        mypre.innerHTML = mypre.innerHTML +  "\n>"; 
        $('#output').scrollTop($('#output')[0].scrollHeight);
-   });
-     
+       lastCompileSuccessful = false;
+    });
 } 
 
 function editorChange(changeObj) {
